7c7f83f6d185df9427303a02285397dbd39871e1
PutraGandaD <putraganda.dewata@gmail.com>
2025-01-16 02:07:32 +0700

    feat(home) : Use Job for proper cancelling coroutines

--- feature/home/src/main/java/com/dicoding/tourismapp/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/dicoding/tourismapp/feature/home/HomeViewModel.kt
@@ -6,8 +6,12 @@ import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
 import com.dicoding.tourismapp.core.utils.ConnectivityManager
 import com.dicoding.tourismapp.core.utils.Resource
 import com.dicoding.tourismapp.feature.home.HomeUiState
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -18,34 +22,52 @@ class HomeViewModel(
     private val _tourismData = MutableStateFlow(HomeUiState())
     val tourismData = _tourismData.asStateFlow()
 
+    private var getTourismDataJob: Job? = null
+
     init {
         getTourismData()
     }
 
-    fun getTourismData() = viewModelScope.launch {
-        checkInternetConnection()
-        tourismUseCase.getAllTourism().collect { result ->
-            when(result) {
-                is Resource.Success -> {
-                    _tourismData.update { currentUiState ->
-                        currentUiState.copy(isLoading = false, data = result.data, message = null)
-                    }
-                }
+    fun getTourismData() {
+        getTourismDataJob?.cancel()
+        getTourismDataJob = viewModelScope.launch {
+            if (connectivityManager.hasInternetConnection()) {
+                hasInternetConnection(true)
+                tourismUseCase.getAllTourism().collect { result ->
+                    when (result) {
+                        is Resource.Success -> {
+                            _tourismData.update { currentUiState ->
+                                currentUiState.copy(
+                                    isLoading = false,
+                                    data = result.data,
+                                    message = null
+                                )
+                            }
+                        }
 
-                is Resource.Error -> {
-                    _tourismData.update { currentUiState ->
-                        currentUiState.copy(isLoading = false, data = result.data, message = result.message)
-                    }
-                }
+                        is Resource.Error -> {
+                            _tourismData.update { currentUiState ->
+                                currentUiState.copy(
+                                    isLoading = false,
+                                    data = result.data,
+                                    message = result.message
+                                )
+                            }
+                        }
 
-                is Resource.Loading -> {
-                    _tourismData.update { currentUiState ->
-                        currentUiState.copy(isLoading = true, data = null, message = null)
+                        is Resource.Loading -> {
+                            _tourismData.update { currentUiState ->
+                                currentUiState.copy(isLoading = true, data = null, message = null)
+                            }
+                        }
                     }
                 }
+            } else {
+                hasInternetConnection(false)
             }
         }
     }
+
 
     fun messageShowed() = viewModelScope.launch {
         _tourismData.update { currentUiState ->
@@ -53,15 +75,9 @@ class HomeViewModel(
         }
     }
 
-    fun checkInternetConnection() = viewModelScope.launch {
-        if(connectivityManager.hasInternetConnection()) {
-            _tourismData.update { currentUiState ->
-                currentUiState.copy(hasInternetConnection = true)
-            }
-        } else {
-            _tourismData.update { currentUiState ->
-                currentUiState.copy(hasInternetConnection = false)
-            }
+    fun hasInternetConnection(boolean: Boolean) {
+        _tourismData.update { currentUiState ->
+            currentUiState.copy(hasInternetConnection = boolean)
         }
     }
 
